a052678ec6a6714a796162a1e32ede23d2fff595
thk88442 <tk88442@gmail.com>
2018-10-09 18:02:31 -0400

    Updates

---
+++ precache-manifest.17da02c4e8b57dc96482426300968371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-Builder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "6ac138047ea8d4fdfd7d",
+    "url": "/Burger-Builder/static/js/runtime~main.6ac13804.js"
+  },
+  {
+    "revision": "01073ef5fee52e7a35b2",
+    "url": "/Burger-Builder/static/js/main.01073ef5.chunk.js"
+  },
+  {
+    "revision": "46780b80980aa0914501",
+    "url": "/Burger-Builder/static/js/1.46780b80.chunk.js"
+  },
+  {
+    "revision": "01073ef5fee52e7a35b2",
+    "url": "/Burger-Builder/static/css/main.476785c3.chunk.css"
+  },
+  {
+    "revision": "f0b9e6121a7e3ff313c3b4c66b165dbc",
+    "url": "/Burger-Builder/index.html"
+  }
+];
